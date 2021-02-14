e3b22a6a8e2bbfe083d14a0049bb6b4a8c3ed260
Juan Henriquez <juanjoaquino05@gmail.com>
2021-02-13 22:09:37 -0400

    Adding variables

--- read_page.sh
+++ read_page.sh
@@ -1,7 +1,9 @@
 # Getting the HTML of Page 
+PAGE="www.wikipedia.org"
+PORT=80
 
 # Getting page
-exec 3<>/dev/tcp/www.wikipedia.org/80
+exec 3<>/dev/tcp/$PAGE/$PORT
 echo -e "GET / HTTP/1.1\n\n" >&3
 cat <&3
 
